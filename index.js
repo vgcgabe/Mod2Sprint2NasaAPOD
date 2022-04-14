94f5a2add4e843044e599274c0a07bfee9420446
vgcgabe <v.g.cguedes@gmail.com>
2022-04-14 19:43:41 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const descricao = $("#descricaoImagem");
+const titulo = $("#title");
+const imagem = $("#imagem");
+//const botao = $("#botaoEnviaData");
+const video = $("#video");
+const chave = 'W9B28XjCqYfGaLBKfX4iaXezkfeylrT8zv4n0roZ';
+
+$("#botaoEnviaData").click(function(event){
+    event.preventDefault();
+    const date = $("#date").val();
+
+    $.ajax({
+        url: `https://api.nasa.gov/planetary/apod?api_key=${chave}&date=${date}`,
+        
+        success: function(resposta){
+            descricao.text(resposta.explanation);
+            titulo.text(resposta.title);
+
+            if(resposta.media_type == 'image'){
+                imagem.show();
+                imagem.attr("src", resposta.url);
+                video.hide();
+            }else{
+                video.show();
+                video.attr("src", resposta.url);
+                imagem.hide();
+            }
+
+        }
+    });
+});
